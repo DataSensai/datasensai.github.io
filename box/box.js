504d09ac0785e3e67fd5e34c220e92043dfd0453
DataSensai <111962014+DataSensai@users.noreply.github.com>
2022-08-24 18:07:04 +0200

    Zusätzlicher Div-Container Test

--- box/box.js
+++ box/box.js
@@ -41,17 +41,17 @@
 		</style>
 		
 		<div class="container">
-		  <div class="row">
-		    <div class="col-md-4 col-sm-4">
-		      <div class="metric participation" data-ratio=".95">
-		        <svg viewBox="0 0 1000 500">
-			        <path d="M 950 500 A 450 450 0 0 0 50 500"></path>
-					<text class='percentage' text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">0%</text>
-					<text class='title' text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal"></text>
-  	            </svg>
-		      </div>
-		    </div>
-		  </div>
+		  //<div class="row">
+		    //<div class="col-md-4 col-sm-4">
+		      //<div class="metric participation" data-ratio=".95">
+		        //<svg viewBox="0 0 1000 500">
+			      //  <path d="M 950 500 A 450 450 0 0 0 50 500"></path>
+					//<text class='percentage' text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">0%</text>
+					//<text class='title' text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal"></text>
+  	            //</svg>
+		      //</div>
+		    //</div>
+		  //</div>
 		         <div id="myDiagramDiv" style="height: 400px; width: 400px; border: 1px solid black;">Hello World</div>
 		  
 		</div>
